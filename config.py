29114dd8972ac0f2fd2971bff6388eb5a0c985ed
kyle <khomman5265@gmail.com>
2019-11-27 15:01:25 -0500

    Added data export functionality for stations and hkresults

--- config.py
+++ config.py
@@ -8,10 +8,13 @@ class Config(object):
     if not os.path.isdir(f'{base_dir}/plots'):
         os.mkdir(f'{base_dir}/plots')
 
+    if not os.path.isdir(f'{base_dir}/exports'):
+        os.mkdir(f'{base_dir}/exports')
+
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(base_dir,'db/rftns.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     DATA_ARCHIVE_STRUCTURE = "DATA/NET/STA/FILTER"
 
     BASE_PLOT_PATH = f"{os.path.join(base_dir, 'plots/')}"
-
+    BASE_EXPORT_PATH = f"{os.path.join(base_dir, 'exports/')}"
